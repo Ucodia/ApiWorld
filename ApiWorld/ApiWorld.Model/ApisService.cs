b6cc59741e2645418ecd68b4805a63153b47f9a2
Lionel Ringenbach <ucodia@live.com>
2013-11-07 11:42:29 +0100

    Added model and api service

---
+++ ApiWorld/ApiWorld.Model/ApisService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using unirest_net.http;
+
+namespace ApiWorld.Model
+{
+    public class ApisService
+    {
+        public async Task<IList<Api>> GetApis()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
