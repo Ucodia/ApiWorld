b1c4cc42a43aaed092859324665f25c55df08ed6
Lionel Ringenbach <ucodia@live.com>
2013-11-17 16:51:22 +0100

    Added WPF app codebase

---
+++ ApiWorld/ApiWorld.WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ApiWorld.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApiWorld.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ApisService _apiService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _apiService = new ApisService();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var apis = await _apiService.GetApis();
+
+            // TODO: Clear and load elements into list
+        }
+    }
+}
