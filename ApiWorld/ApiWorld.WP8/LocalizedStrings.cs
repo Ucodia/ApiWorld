3aa3d11404c3ce069e3913683897c2c4640b7197
Lionel Ringenbach <ucodia@live.com>
2013-11-17 17:14:43 +0100

    Added WP8 and copied WPF codebase

---
+++ ApiWorld/ApiWorld.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ApiWorld.WP8.Resources;
+
+namespace ApiWorld.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ ApiWorld/ApiWorld.WP8/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ApiWorld.WP8.Resources;
+using ApiWorld.Model;
+using System.Collections.ObjectModel;
+
+namespace ApiWorld.WP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private ApisService _apiService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            _apiService = new ApisService();
+            Apis = new ObservableCollection<Api>();
+        }
+
+        public ObservableCollection<Api> Apis { get; set; }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var apis = await _apiService.GetApis();
+
+            Apis.Clear();
+
+            foreach (var api in apis)
+            {
+                Apis.Add(api);
+            }
+        }
+    }
+}
