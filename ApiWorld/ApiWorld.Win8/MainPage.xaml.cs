d3f4f8f314008fa9c1167e6d4097c9a3955135a2
Lionel Ringenbach <ucodia@live.com>
2013-11-20 23:06:22 +0100

    Copied WPF app changes to Win8

--- ApiWorld/ApiWorld.Win8/MainPage.xaml.cs
+++ ApiWorld/ApiWorld.Win8/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +23,8 @@ namespace ApiWorld.Win8
     public sealed partial class MainPage : Page
     {
         private ApisService _apiService;
+        private int _offset = 0;
+        private const int Limit = 20;
 
         public MainPage()
         {
@@ -30,17 +33,40 @@ namespace ApiWorld.Win8
             _apiService = new ApisService();
         }
 
-        public ObservableCollection<Api> Apis { get; set; }
-
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            var result = await _apiService.GetApis();
+            // Disable button
+            LoadButton.IsEnabled = false;
+            LoadButton.Content = "Loading...";
 
-            ApiListBox.Items.Clear();
-
-            foreach (var api in result.Apis)
+            try
             {
-                ApiListBox.Items.Add(api);
+                // Call API service
+                var result = await _apiService.GetApis(limit: Limit, offset: _offset);
+
+                // Load items in list
+                foreach (var api in result.Apis)
+                {
+                    ApiListBox.Items.Add(api);
+                }
+
+                // Update offset
+                _offset += Limit;
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format(
+                    "Oops, something went wrong. Here's the ugly details:\n{0}",
+                    ex.Message);
+
+                var dialog = new MessageDialog(message);
+                //await dialog.ShowAsync();
+            }
+            finally
+            {
+                // Reenable button
+                LoadButton.IsEnabled = true;
+                LoadButton.Content = string.Format("Load {0} more items", Limit);
             }
         }
     }
