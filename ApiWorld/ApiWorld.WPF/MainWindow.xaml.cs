fed86e9b232fb0ae07638643e1ed8734eb8ae273
Lionel Ringenbach <ucodia@live.com>
2013-11-20 22:55:06 +0100

    Added offset handling to load next results instead of clearing list

--- ApiWorld/ApiWorld.WPF/MainWindow.xaml.cs
+++ ApiWorld/ApiWorld.WPF/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace ApiWorld.WPF
     public partial class MainWindow : Window
     {
         private ApisService _apiService;
-
+        private int _offset = 0;
+        private const int Limit = 20;
 
         public MainWindow()
         {
@@ -31,19 +32,23 @@ namespace ApiWorld.WPF
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Disable button
             LoadButton.IsEnabled = false;
             LoadButton.Content = "Loading...";
 
             try
             {
-                var result = await _apiService.GetApis();
+                // Call API service
+                var result = await _apiService.GetApis(limit:Limit, offset:_offset);
 
-                ApiListBox.Items.Clear();
-
+                // Load items in list
                 foreach (var api in result.Apis)
                 {
                     ApiListBox.Items.Add(api);
                 }
+
+                // Update offset
+                _offset += Limit;
             }
             catch (Exception ex)
             {
@@ -55,8 +60,9 @@ namespace ApiWorld.WPF
             }
             finally
             {
+                // 
                 LoadButton.IsEnabled = true;
-                LoadButton.Content = "Load";
+                LoadButton.Content = string.Format("Load {0} more items", Limit);
             }
         }
     }
